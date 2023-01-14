036f1fc5049b31a366b0d5f84b4db77de09c361c
Raya Ghaniyya <rayaghaniyyayuda@gmail.com>
2023-01-14 16:37:24 +0700

    update

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,7 @@
 $server = "localhost";
 $user = "root";
 $password = "";
-$nama_database = "inventory barang";
+$nama_database = "inventory_barang";
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
