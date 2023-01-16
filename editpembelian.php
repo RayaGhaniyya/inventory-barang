d6b676c4f9aaeec460d75d2353399c6631bbe0f7
raditfirmanzah <116476187+raditfirmanzah@users.noreply.github.com>
2023-01-16 13:42:27 +0700

    update all

--- editpembelian.php
+++ editpembelian.php
@@ -5,16 +5,19 @@ include("head.php");
 
 
 if (!isset($_GET['id'])) {
-    header('Location: listuser.php');
+    header('Location: listpembelian.php');
 }
 
 
 $id = $_GET['id'];
 
 
-$sql = "SELECT * FROM user WHERE id=$id";
+$sql = "SELECT * FROM pembelian WHERE id=$id";
 $query = mysqli_query($db, $sql);
-$user = mysqli_fetch_assoc($query);
+$pembelian = mysqli_fetch_assoc($query);
+$sqlbarang = "SELECT * FROM barang";
+$querybarang = mysqli_query($db, $sqlbarang);
+
 
 
 if (mysqli_num_rows($query) < 1) {
@@ -35,38 +38,45 @@ if (mysqli_num_rows($query) < 1) {
 
     <p>
         <b>
-            Form Edit Customer
+            Form Edit Pembelian
         </b>
     </p>
 </header>
 
 <div class="container">
-    <form action="prosesedituser.php" method="POST">
-        <input type="hidden" name="id" value="<?php echo $user['id'] ?>" />
+    <form action="proseseditpembelian.php" method="POST">
+        <input type="hidden" name="id" value="<?php echo $pembelian['id'] ?>" />
         <div class="container">
-            <div class="card card-user-edit">
+            <div class="card card-pembelian-edit">
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-6">
 
-                            <label for="nama">Nama :</label>
-                            <input class="form-control" type="text" name="nama" placeholder="Nama" aria-label="default input example" value="<?php echo $user['nama'] ?>">
+                            <label for="kode_pembelian">Kode Pembelian :</label>
+                            <input class="form-control" type="text" name="kode_pembelian" placeholder="kode_pembelian" aria-label="default input example" value="<?php echo $pembelian['kode_pembelian'] ?>">
                         </div>
                         <div class="col-md-6">
 
-                            <label for="username">Username:</label>
-                            <input class="form-control" type="text" name="username" placeholder="Username" aria-label="default input example" value="<?php echo $user['username'] ?>">
+                        <label for="id_barang">Pilih Barang: </label>
+                        <select name="id_barang" class="form-control" aria-label="default input example">
+                            <option value="">Pilih Barang</option>
+                            <?php
+                            while ($barang = mysqli_fetch_assoc($querybarang)) {
+                                $selected = $barang['id_barang'] == $barang['id_barang'] ? 'selected="selected"' : '';
+                                echo "<option value=" . $barang['id_barang'] . " $selected>" . $barang['id_barang'] . "</option>";
+                            } ?>
+                        </select>
+                           
+                        </div>
+                        <div class="col-md-6">
+                            <label for="jumlah">Jumlah :</label>
+                            <input class="form-control" type="text" name="jumlah" placeholder="Jumlah" aria-label="default input example" value="<?php echo $pembelian['jumlah'] ?>">
 
                         </div>
                         <div class="col-md-6">
-                            <label for="no_hp">No Hp :</label>
-                            <input class="form-control" type="text" name="no_hp" placeholder="No Hp" aria-label="default input example" value="<?php echo $user['no_hp'] ?>">
 
-                        </div>
-                        <div class="col-md-6">
-
-                            <label for="password">Password :</label>
-                            <input class="form-control" type="password" name="password" placeholder="password" aria-label="default input example" value="<?php echo $user['password'] ?>">
+                            <label for="harga">Harga:</label>
+                            <input class="form-control" type="harga" name="harga" placeholder="harga" aria-label="default input example" value="<?php echo $pembelian['harga'] ?>">
                         </div>
                         <div class="col-md-12">
 
---
+++ listpembelian.php
@@ -0,0 +1,77 @@
+<?php
+include("koneksi.php");
+include('head.php');
+?>
+<header>
+    <p>
+        <b>LIST USER</b>
+    </p>
+</header>
+
+<div class="table-user">
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
+                <tr class="table-secondary">
+                    <th>Id</th>
+                    <th>Kode Pembelian</th>
+                    <th>id barang</th>
+                    <th>Jumlah</th>
+                    <th>Harga</th>
+                    <th>Tindakan</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+           
+           $sql = "SELECT pembelian.id,pembelian.kode_pembelian,pembelian.id_barang,pembelian.jumlah,pembelian.harga FROM pembelian LEFT JOIN barang ON pembelian.id_barang=barang.id";
+           $query = mysqli_query($db, $sql);
+
+
+           while ($pembelian = mysqli_fetch_array($query)) {
+               echo "<tr>";
+
+               echo "<td>" . $pembelian['id'] . "</td>";
+               echo "<td>" . $pembelian['kode_pembelian'] . "</td>";
+               echo "<td>" . $pembelian['id_barang'] . "</td>";
+               echo "<td>" . $pembelian['jumlah'] . "</td>";
+               echo "<td>" . $pembelian['harga'] . "</td>";
+             
+
+
+
+               echo "<td>";
+               echo '<a href=editpembelian.php?id=' . $pembelian['id'] . ' class="btn btn-warning">Edit</a> | ';
+               echo '<a href="hapuspembelian.php?id=' . $pembelian['id'] . '" class="btn btn-danger">Hapus</a> |';
+               echo '<a href=detailpembelian.php?id=' . $pembelian['id'] . ' class="btn btn-info">Detail</a>  ';
+               echo "</td>";
+
+               echo "</tr>";
+           }
+           ?>
+       </tbody>
+
+   </table>
+</div>
+</div>
+
+<div class="container text-center">
+<div class="row justify-content-center">
+   <div class="col-2 pt-4 pe-5">
+       <p>Total: <?php echo mysqli_num_rows($query) ?></p>
+   </div>
+   <div class="col-3">
+       <nav>
+           <a href="formpembelian.php" class="btn btn-light" style="margin-bottom: 1rem; margin-top:1rem; margin-left:2rem;">[+] Tambah Baru</a>
+       </nav>
+   </div>
+</div>
+</div>
+
+<?php
+include('bottom.php');
+?>
+<!-- </body>
+
+</html> -->
