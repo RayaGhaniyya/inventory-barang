c05beb05e3069170232c814516ba3436a42d1020
Raya Ghaniyya <rayaghaniyyayuda@gmail.com>
2023-01-16 17:49:06 +0700

    update index

--- index.php
+++ index.php
@@ -121,15 +121,3 @@ include('head.php');
 <?php
 include('bottom.php');
 ?>
-
-<?php if (isset($_GET['status'])) : ?>
-  <p class="status" style="text-align: center; font-size: xx-large;">
-    <?php
-    if ($_GET['status'] == 'sukses') {
-      echo "Pendaftaran berhasil!";
-    } else {
-      echo "Pendaftaran gagal!";
-    }
-    ?>
-  </p>
-<?php endif; ?>
---
+++ index2.php
@@ -0,0 +1,21 @@
+<?php
+
+include('head.php');
+?>
+
+
+<?php
+include('bottom.php');
+?>
+
+<?php if (isset($_GET['status'])) : ?>
+    <p class="status" style="text-align: center; font-size: xx-large;">
+        <?php
+        if ($_GET['status'] == 'sukses') {
+            echo "Pendaftaran berhasil!";
+        } else {
+            echo "Pendaftaran gagal!";
+        }
+        ?>
+    </p>
+<?php endif; ?>
