e306471a24d062d87e87139dadafbb4d83b9a6b0
Raya Ghaniyya <rayaghaniyyayuda@gmail.com>
2023-01-18 14:03:47 +0700

    update foto

--- index.php
+++ index.php
@@ -13,88 +13,35 @@ include('head.php');
   <div class="row pt-3">
     <div class="col-6">
 
-      <div id="myCarousel" class="carousel slide vertical">
-        <!-- Carousel items -->
-        <div class="carousel-inner">
-          <div class="active item">
-            <img src="gambar/idm1.jpg">
-          </div>
-          <div class="item">
-            <img src="gambar/idm2.jpg">
-          </div>
-          <div class="item">
-            <img src="gambar/idm3.jpg">
-          </div>
-        </div>
+      <div class="slide-wrapper">
+        <input type="radio" name="slider" id="slider1" checked>
+        <label for="slider1"></label>
+        <div class="slide slide1"></div>
 
-        <!-- Carousel nav -->
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+        <input type="radio" name="slider" id="slider2">
+        <label for="slider2"></label>
+        <div class="slide slide2"></div>
 
+        <input type="radio" name="slider" id="slider3">
+        <label for="slider3"></label>
+        <div class="slide slide3"></div>
       </div>
-
     </div>
-  </div>
-  <!-- <div id="myCarousel" class="carousel slide vertical"> -->
-  <!-- Carousel items -->
-  <!-- <div class="carousel-inner">
-      <div class="active item">
-        <img src="http://placehold.it/1170x400&amp;text=First+Slide">
-      </div>
-      <div class="item">
-        <img src="http://placehold.it/1170x400&amp;text=Second+Slide">
-      </div>
-      <div class="item">
-        <img src="http://placehold.it/1170x400&amp;text=Third+Slide">
-      </div>
-    </div> -->
 
-  <!-- <div id="carouselExampleIndicators" class="carousel slide vertical">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="/inventory-barang/gambar/idm3.jpg" class="idm">
-          </div>
-          <div class="carousel-item">
-            <img src="/inventory-barang/gambar/idm1.jpg" class="idm">
-          </div>
-          <div class="carousel-item">
-            <img src="/inventory-barang/gambar/idm2.jpg" class="idm">
-          </div>
-        </div> -->
-
-  <!-- <a class="left carousel-control" href="#myCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a> -->
-
-  <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button> -->
-  <!-- </div>
-    </div> -->
-
-  <div class="col-6" style="color: white;
+    <div class="col-6" style="color: white;
     text-align: start;
     padding-top: 5rem;
     padding-left: 3rem;">
-    <h1>
-      PT Indomarco Prismatama
-    </h1>
-    <h5>
-      Berbekal dedikasi dan inovasi, Indomaret mengukuhkan statusnya sebagai perusahaan waralaba minimarket pertama dan terbesar di Indonesia
-    </h5>
+      <h1>
+        PT Indomarco Prismatama
+      </h1>
+      <h5>
+        Berbekal dedikasi dan inovasi, Indomaret mengukuhkan statusnya sebagai perusahaan waralaba minimarket pertama dan terbesar di Indonesia
+      </h5>
+    </div>
   </div>
+</div>
 
-</div>
-</div>
 <?php
 include('bottom.php');
 ?>
