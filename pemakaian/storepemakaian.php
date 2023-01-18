4f8e477a9425d26b3c21b4060b7b2479ff663278
raditfirmanzah <116476187+raditfirmanzah@users.noreply.github.com>
2023-01-18 14:11:26 +0700

    update pemakaian.php

--- pemakaian/storepemakaian.php
+++ pemakaian/storepemakaian.php
@@ -11,25 +11,33 @@ if (isset($_POST['daftar'])) {
     $id_barang = $_POST['id_barang'];
     $jumlah = $_POST['jumlah'];
 
-
-    // 
-    $sql =  "INSERT INTO pemakaian (kode_pemakaian, id_barang, jumlah) VALUE ('$kode_pemakaian', '$id_barang', '$jumlah')";
-    $query = mysqli_query($db, $sql);
-
-
-    //
-    //
-
-    $sqlupdatestok = "UPDATE barang SET jumlh_stok = jumlh_stok - $jumlah WHERE id = $id_barang;";
-    $query = mysqli_query($db, $sqlupdatestok);
-    // 
-    if ($query) {
-        // 
-        header('Location: ../index2.php?status=sukses');
+    $cekstok = mysqli_query($db, "SELECT barang.jumlh_stok FROM barang WHERE id=$id_barang");
+    $stok = mysqli_fetch_array($cekstok);
+    if($jumlah > $stok['jumlh_stok']){
+        echo " <script>
+        alert('Stok gak cukup karena anda blom sholat !!!  Silahkan sholat terlebih dahulu');window.location.href='/inventory-barang/pemakaian/formpemakaian.php';
+        </script>";
+        
     } else {
-        //
-        header('Location: ../index2.php?status=gagal');
+        
+        $sql =  "INSERT INTO pemakaian (kode_pemakaian, id_barang, jumlah) VALUE ('$kode_pemakaian', '$id_barang', '$jumlah')";
+        $query = mysqli_query($db, $sql);
+    
+        $sqlupdatestok = "UPDATE barang SET jumlh_stok = jumlh_stok - $jumlah WHERE id = $id_barang;";
+        $query = mysqli_query($db, $sqlupdatestok);
+        // 
+        if ($query) {
+            // 
+            header('Location: ../index2.php?status=sukses');
+        } else {
+            //
+            header('Location: ../index2.php?status=gagal');
+        }
     }
+     
+
 } else {
     die("Akses dilarang...");
 }
+
+?>
